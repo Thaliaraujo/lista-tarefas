32d8356e876cabed6ae7dc3b5ecaf69e0b62eaa2
thaliaraujo <thalia.araujo321@gmail.com>
2023-06-29 16:34:03 -0300

    first commit

---
+++ componentes/deletaTarefa.js
@@ -0,0 +1,22 @@
+const BotaoDeleta = () => { 
+    const botaoDeleta = document.createElement('button')
+
+    botaoDeleta.classList.add('delete-button')
+    botaoDeleta.innerText = 'Deletar'
+    botaoDeleta.addEventListener('click', deletarTarefa)
+
+    return botaoDeleta
+}
+
+const deletarTarefa = (evento) => { 
+    const botaoDeleta = evento.target
+    
+    const tarefaCompleta = botaoDeleta.parentElement
+
+    tarefaCompleta.remove()
+
+    return botaoDeleta
+
+}
+
+export default BotaoDeleta
